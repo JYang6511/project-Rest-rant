46e02fd26c0d3d743afa54e8430e792408f37fa1
Johnson Yang <johnsonyang65@gmail.com>
2023-10-14 19:04:19 -0700

    style

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                <img src="/images/breathe404.jpg" alt="breathe sign"/>
+                <div>
+                  Photo by <a href="https://unsplash.com/@maxvdo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Max van den Oetelaar</a> on <a href="https://unsplash.com">Unsplash</a>
+                </div>
+              </div>
           </main>
       </Def>
     )
