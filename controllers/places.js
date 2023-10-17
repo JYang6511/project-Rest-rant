2700db083b3a23d3c6f82ee0bdb827c1af8b8fe0
Johnson Yang <johnsonyang65@gmail.com>
2023-10-16 21:28:45 -0700

    update

--- controllers/places.js
+++ controllers/places.js
@@ -4,6 +4,11 @@ const places = require('../models/places.js')
 
 
 
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+})
+
+
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
@@ -23,9 +28,6 @@ router.get('/:id', (req, res) => {
 })
 
 
-router.get('/', (req, res) => {
-  res.render('places/index', { places })
-})
 
 router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
