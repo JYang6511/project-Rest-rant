aada51bfee303a6031f36154e645ed07c1ba1331
Johnson Yang <johnsonyang65@gmail.com>
2023-10-27 19:54:52 -0700

    got comments to show

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+          return (
+            <div className="border">
+              <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+              <h4>{c.content}</h4>
+              <h3>
+                <stong>- {c.author}</stong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+            </div>
+          )
+        })
+      }
     return (
         <Def>
           <main>
@@ -29,7 +48,7 @@ function show (data) {
             </div>
             <div>
                 <h2>Comments</h2>
-                <p>No Comments yet!</p>
+                {comments}
                     <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                         Edit
                     </a> 
