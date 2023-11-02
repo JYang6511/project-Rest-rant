c8f457821cb515cd87cffdf4582b4f2acde20b8c
Johnson Yang <johnsonyang65@gmail.com>
2023-11-02 14:16:54 -0700

    fixed edit and delete route

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -213,10 +213,10 @@ function show(data) {
 
         <hr />
         <div className="showButtonDiv">
-          <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+          <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
             Edit
           </a>
-          <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+          <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
             <button type="submit" className="btn btn-danger">
               Delete
             </button>
